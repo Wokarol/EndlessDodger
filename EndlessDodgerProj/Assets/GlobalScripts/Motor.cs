462f62763b46d40a1e19cc2d476bc1a12ee235e7
Wokarol <wojtek.karolczak20@gmail.com>
2018-06-08 17:57:07 +0200

    Added acceleration;

--- EndlessDodgerProj/Assets/GlobalScripts/Motor.cs
+++ EndlessDodgerProj/Assets/GlobalScripts/Motor.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 namespace Wokarol {
 	public class Motor : MonoBehaviour {
 		[SerializeField] float speed;
+		[SerializeField] float acceleration;
 
 		void FixedUpdate () {
 			transform.Translate(transform.InverseTransformVector(Vector3.up) * speed * Time.fixedDeltaTime);
+			speed += acceleration;
 		}
 	}
 }

8378b72dd97f1de24ee9fb7ef5571d8f4de049db
Wokarol <wojtek.karolczak20@gmail.com>
2018-06-28 19:06:39 +0200

    Changed serialization system to binary serialization

--- EndlessDodgerProj/Assets/ScoreSystem/ScoreSystem.cs
+++ EndlessDodgerProj/Assets/ScoreSystem/ScoreSystem.cs
@@ -6,7 +6,12 @@ using UnityEngine.Events;
 namespace Wokarol.ScoreSystem {
 	[CreateAssetMenu]
 	public class ScoreSystem : ScriptableObject {
+
+		[Tooltip("Serializer object that deserializes file containg highscore")]
+		[SerializeField] Serializer HighscoreData;
+
 		public int Score { get; private set; }
+
 		public UnityEvent OnHighscore;
 
 		public void SetScore (int value)
@@ -20,9 +25,12 @@ namespace Wokarol.ScoreSystem {
 
 		public void CountHighscore ()
 		{
-			var prevHighscore = PlayerPrefs.GetFloat(StringConsts.HIGHSCORE);
+			var prevHighscore = HighscoreData.GetEntry<int>(StringConsts.HIGHSCORE, 0);
+			//var prevHighscore = PlayerPrefs.GetFloat(StringConsts.HIGHSCORE);
+
 			if(Score > prevHighscore) {
-				PlayerPrefs.SetFloat(StringConsts.HIGHSCORE, Score);
+				//PlayerPrefs.SetFloat(StringConsts.HIGHSCORE, Score);
+				HighscoreData.SendEntry(StringConsts.HIGHSCORE, Score);
 				OnHighscore.Invoke();
 			}
 		}

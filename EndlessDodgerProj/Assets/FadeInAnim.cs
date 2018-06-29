334a76d0a9c21f66ab23f30909394170361ac8e9
Wokarol <wojtek.karolczak20@gmail.com>
2018-06-29 13:26:33 +0200

    [v0.7.0] Moved some files; Added new menu assets;

---
+++ EndlessDodgerProj/Assets/FadeInAnim.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Wokarol {
+	[RequireComponent(typeof(CanvasGroup))]
+	public class FadeInAnim : MonoBehaviour {
+		CanvasGroup group;
+		float progress;
+
+		[Tooltip("Seconds nedded to fade in")]
+		[SerializeField] float time = 1;
+
+		private void Awake ()
+		{
+			group = GetComponent<CanvasGroup>();
+			progress = 0;
+		}
+
+		private void Update ()
+		{
+			group.alpha = 1 - progress;
+			progress = Mathf.Clamp01(progress += Time.deltaTime * (1/time));
+		}
+
+		private void OnValidate ()
+		{
+			progress = 0;
+		}
+	}
+}

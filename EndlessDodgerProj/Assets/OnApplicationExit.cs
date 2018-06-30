0983b494acf23f59fcf63d43a0823d7a2d204f3b
Wokarol <wojtek.karolczak20@gmail.com>
2018-06-30 21:31:38 +0200

    Added OnApplicationExit

---
+++ EndlessDodgerProj/Assets/OnApplicationExit.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Wokarol {
+	public class OnApplicationExit : MonoBehaviour {
+		[SerializeField] UnityEvent exitEvent;
+
+		private void OnApplicationQuit ()
+		{
+			exitEvent.Invoke();
+		}
+	}
+}
